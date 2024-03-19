41957276c368ec965f7dd805f671de8e5961a552
eppdalencon <eppdalencon@gmail.com>
2024-03-19 14:41:07 -0300

    Setup básico para ViewCode

---
+++ Movies/Movies/ListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListViewController.swift
+//  Movies
+//
+//  Created by Eduardo Dalencon on 19/03/24.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("build")
+        setup()
+       
+    }
+
+}
+
+
+private extension ListViewController {
+    
+    private func setup() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.title = "MovieDB"
+        self.view.backgroundColor = .white
+        }
+    
+}
