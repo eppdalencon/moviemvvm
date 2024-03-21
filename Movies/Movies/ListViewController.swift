ad03caa66779da79c7cf1f125c0d31e71644af31
Lucas Cunha <lucascunha@MacBook-Pro-de-Lucas-Cunha.local>
2024-03-21 11:23:57 -0700

    view

--- Movies/Movies/ListViewController.swift
+++ Movies/Movies/ListViewController.swift
@@ -22,17 +22,27 @@ private extension ListViewController {
     
     private func setup() {
         
+        let scrollView = UIScrollView()
         let vw = MovieDetailsView()
-        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         vw.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(vw)
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(vw)
         
         NSLayoutConstraint.activate([
             vw.topAnchor.constraint(equalTo: view.topAnchor, constant: 8),
             vw.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8),
             vw.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
             vw.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
         ])
         
